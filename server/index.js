1c858ec4a0a9cb4223dc58208f3484f8efff4dca
Sarwar Nazrul <sarwar.nazrul242@gmail.com>
2024-11-18 17:36:24 -0500

    Live Server 2

--- server/index.js
+++ server/index.js
@@ -6,10 +6,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
+app.use(cors());
 
 
 /*NEW STUFF*/
